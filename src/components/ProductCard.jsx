bbdf5eaa778f5f6d1dff44379f0724337c791426
Imoyemi <okeibrahim133@gmail.com>
2025-08-12 15:05:32 -0500

    feat: access props to display product info

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,19 @@
-export default function ProductCard({ bgImg, name }) {
+export default function ProductCard({ bgImg, name, category, price }) {
   //   console.log();
   return (
     <article>
       <picture>
-        <source media='()' />
+        <source media='(min-width:1024px)' srcSet={bgImg.desktop} />
+        <source media='(min-width:768px)' srcSet={bgImg.tablet} />
+        <img src={bgImg.mobile} alt={name} className='w-full lg' />
       </picture>
-      <img src={bgImg.desktop} alt={name} className='w-full' />
+      <span>{category}</span>
+      <p>{name}</p>
+      <p>${price}</p>
+      <div>
+        <img src='./public/images/icon-add-to-cart.svg' alt='cart' />
+        <span>Add to Card</span>
+      </div>
     </article>
   );
 }
