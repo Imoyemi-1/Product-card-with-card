bfce60b297eb28141fe1269e633d01115967c579
Imoyemi <okeibrahim133@gmail.com>
2025-08-12 14:15:41 -0500

    feat: get product detail and add props to card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import ProductCard from './components/ProductCard';
+import { Products } from './data/product';
+
 function App() {
-  return <></>;
+  const cards = Products.map((product) => {
+    return (
+      <ProductCard
+        key={product.id}
+        bgImg={product.image}
+        category={product.category}
+        name={product.name}
+        price={product.price}
+      />
+    );
+  });
+  return (
+    <>
+      <h1 className='text-4xl font-bold'>Desserts</h1>
+      <main className='grid gap-2'>{cards}</main>
+    </>
+  );
 }
 
 export default App;
