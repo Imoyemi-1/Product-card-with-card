43a2c72eec89c2243f0947f024ee6cef0580becf
Imoyemi <okeibrahim133@gmail.com>
2025-08-12 17:03:32 -0500

    feat: create cart component

---
+++ src/components/Cart.jsx
@@ -0,0 +1,14 @@
+export default function Cart() {
+  return (
+    <section>
+      <h2>Your Cart (0)</h2>
+      <div>
+        <img
+          src='./public/images/illustration-empty-cart.svg'
+          alt='empty-cart'
+        />
+        <p>Your added items will appear here</p>
+      </div>
+    </section>
+  );
+}
