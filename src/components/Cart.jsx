a0e7144af4b03d67f1a17e09f71bab69482b3817
Imoyemi <okeibrahim133@gmail.com>
2025-08-15 08:46:14 -0500

    feat: added  all quantity together

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,11 @@
-export default function Cart() {
+export default function Cart({ cart }) {
+  const totalQuantity = cart.reduce((acc, cur) => acc + cur.quantity, 0);
+
   return (
     <section className='bg-white p-6 rounded-2xl space-y-8'>
-      <h2 className='text-custom-Red font-bold text-2xl'>Your Cart (0)</h2>
+      <h2 className='text-custom-Red font-bold text-2xl'>
+        Your Cart ({totalQuantity})
+      </h2>
       <div className='flex flex-col gap-6 w-full'>
         <img
           src='./public/images/illustration-empty-cart.svg'
